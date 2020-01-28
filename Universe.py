5715d4d16f8fbf5b2233ca67664cb4b08772da73
KosiorekD <57966133+KosiorekD@users.noreply.github.com>
2020-01-27 20:00:30 -0500

    Update Universe.py

--- Universe.py
+++ Universe.py
@@ -1,32 +1,110 @@
-import sys
 from OpenGL.GL import *
-from OpenGL.GLU import *
 from OpenGL.GLUT import *
+from OpenGL.GLU import *
+import serial
+import os
+import threading
 
-# The display() method does all the work; it has to call the appropriate
-# OpenGL functions to actually display something.
-def display():
-    # Clear the color and depth buffers
+ESCAPE = '\033'
+
+window = 0
+
+#rotation
+X_AXIS = 0.0
+Y_AXIS = 0.0
+Z_AXIS = 0.0
+
+DIRECTION = 1
+
+def InitGL(Width, Height):
+    glClearColor(0.0, 0.0, 0.0, 0.0)
+    glClearDepth(1.0)
+    glDepthFunc(GL_LESS)
+    glEnable(GL_DEPTH_TEST)
+    glShadeModel(GL_SMOOTH)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
+    glMatrixMode(GL_MODELVIEW)
+
+def keyPressed(*args):
+    if args[0] == ESCAPE:
+            sys.exit()
+
+def DrawGLScene():
+    global X_AXIS, Y_AXIS, Z_AXIS
+    global DIRECTION
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-    # ... render stuff in here ...
-    # It will go to an off-screen frame buffer.
+    glLoadIdentity()
+    glTranslatef(0.0, 0.0, -6.0)
 
-    # Copy the off-screen buffer to the screen.
+    glRotatef(X_AXIS, 1.0, 0.0, 0.0)
+    glRotatef(Y_AXIS, 0.0, 1.0, 0.0)
+    glRotatef(Z_AXIS, 0.0, 0.0, 1.0)
+
+    # Draw Cube (multiple quads)
+    glBegin(GL_QUADS)
+
+    glColor3f(0.0, 1.0, 0.0)
+    glVertex3f( 1.0, 1.0, -1.0)
+    glVertex3f(-1.0, 1.0, -1.0)
+    glVertex3f(-1.0, 1.0, 1.0)
+    glVertex3f( 1.0, 1.0, 1.0)
+
+    glColor3f(1.0, 0.0, 0.0)
+    glVertex3f( 1.0,-1.0, 1.0)
+    glVertex3f(-1.0,-1.0, 1.0)
+    glVertex3f(-1.0,-1.0,-1.0)
+    glVertex3f( 1.0,-1.0,-1.0)
+
+    glColor3f(0.0, 1.0, 0.0)
+    glVertex3f( 1.0, 1.0, 1.0)
+    glVertex3f(-1.0, 1.0, 1.0)
+    glVertex3f(-1.0,-1.0, 1.0)
+    glVertex3f( 1.0,-1.0, 1.0)
+
+    glColor3f(1.0, 1.0, 0.0)
+    glVertex3f( 1.0,-1.0,-1.0)
+    glVertex3f(-1.0,-1.0,-1.0)
+    glVertex3f(-1.0, 1.0,-1.0)
+    glVertex3f( 1.0, 1.0,-1.0)
+
+    glColor3f(0.0, 0.0, 1.0)
+    glVertex3f(-1.0, 1.0, 1.0)
+    glVertex3f(-1.0, 1.0,-1.0)
+    glVertex3f(-1.0,-1.0,-1.0)
+    glVertex3f(-1.0,-1.0, 1.0)
+
+    glColor3f(1.0, 0.0, 1.0)
+    glVertex3f( 1.0, 1.0,-1.0)
+    glVertex3f( 1.0, 1.0, 1.0)
+    glVertex3f( 1.0,-1.0, 1.0)
+    glVertex3f( 1.0,-1.0,-1.0)
+
+    glEnd()
+
+    X_AXIS = X_AXIS - 0.30
+    Z_AXIS = Z_AXIS - 0.30
+
     glutSwapBuffers()
 
-glutInit(sys.argv)
+def main():
+    global window
 
-# Create a double-buffer RGBA window.   (Single-buffering is possible.
-# So is creating an index-mode window.)
-glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
+    glutInit(sys.argv)
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH)
+    glutInitWindowSize(640,480)
+    glutInitWindowPosition(200,200)
 
-# Create a window, setting its title
-glutCreateWindow('Universe')
+    window = glutCreateWindow('OpenGL Python Cube')
 
-# Set the display callback.  You can set other callbacks for keyboard and
-# mouse events.
-glutDisplayFunc(display)
+    glutDisplayFunc(DrawGLScene)
+    glutIdleFunc(DrawGLScene)
+    glutKeyboardFunc(keyPressed)
+    InitGL(640, 480)
+    glutMainLoop()
 
-# Run the GLUT main loop until the user closes the window.
-glutMainLoop()
+if __name__ == "__main__":
+        main()
