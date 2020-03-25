594c509c96c7de762713d1783ef65502a3341a80
KosiorekD <57966133+KosiorekD@users.noreply.github.com>
2020-03-25 15:03:24 -0400

    Updated "Shapes.py"

--- Shapes.py
+++ Shapes.py
@@ -2,6 +2,14 @@ from OpenGL.GL import *
 from OpenGL.GLUT import *
 from OpenGL.GLU import *
 
+red = [1.0, 0.0, 0.0]
+orange = [1.0, 0.33, 0.0]
+yellow = [1.0, 1.0, 0.0]
+green = [0.0, 1.0, 0.0]
+blue = [0.0, 0.0, 1.0]
+indigo = [0.29, 0.0, 0.51]
+violet = [0.56, 0.0, 1.0]
+
 class Cube:
     def __init__(self, name):
         self.name = name
@@ -37,21 +45,16 @@ class Cube:
                     [ 1.0,-1.0, 1.0],
                     [ 1.0,-1.0,-1.0]]
 
-        colors = [[1.0, 0.0, 0.0],
-                  [1.0, 1.0, 0.0],
-                  [0.0, 0.0, 1.0],
-                  [0.0, 1.0, 1.0],
-                  [1.0, 1.0, 1.0],
-                  [0.0, 1.0, 0.0]]
+        colors = [red, orange, yellow, green, blue, violet]
 
         # Draw the Cube (multiple quads)
         glBegin(GL_QUADS)
 
-        for side in range(6):
-            glColor3f(colors[side][0], colors[side][1], colors[side][2])
-            glVertex3f(vertices[side * 4][0], vertices[side * 4][1], vertices[side * 4][2])
-            glVertex3f(vertices[(side * 4) + 1][0], vertices[(side * 4) + 1][1], vertices[(side * 4) + 1][2])
-            glVertex3f(vertices[(side * 4) + 2][0], vertices[(side * 4) + 2][1], vertices[(side * 4) + 2][2])
-            glVertex3f(vertices[(side * 4) + 3][0], vertices[(side * 4) + 3][1], vertices[(side * 4) + 3][2])
+        for quad in range(6):
+            glColor3f(colors[quad][0], colors[quad][1], colors[quad][2])
+            glVertex3f(vertices[quad * 4][0], vertices[quad * 4][1], vertices[quad * 4][2])
+            glVertex3f(vertices[(quad * 4) + 1][0], vertices[(quad * 4) + 1][1], vertices[(quad * 4) + 1][2])
+            glVertex3f(vertices[(quad * 4) + 2][0], vertices[(quad * 4) + 2][1], vertices[(quad * 4) + 2][2])
+            glVertex3f(vertices[(quad * 4) + 3][0], vertices[(quad * 4) + 3][1], vertices[(quad * 4) + 3][2])
 
         glEnd()
